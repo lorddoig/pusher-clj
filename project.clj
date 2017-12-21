0b6e700633634fb7a819725ebe6931c5217bf530
Sean Doig <sean.doig@style.com>
2017-12-21 11:39:45 +0000

    Add repositories to project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0-RC1"]
-                 [com.pusher/pusher-java-client "1.6.0"]])
+                 [com.pusher/pusher-java-client "1.6.0"]]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]])
