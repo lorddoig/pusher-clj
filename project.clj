b052bde241fbcc5212ceefdd9e568038a0a73fc0
Sean Doig <sean.doig@style.com>
2017-12-21 11:58:16 +0000

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lorddoig/pusher-clj "0.1.0-SNAPSHOT"
+(defproject com.lorddoig/pusher-clj "0.1.0"
   :description "A thin convenience wrapper around the official Pusher Java library."
   :url "http://github.com/lorddoig/pusher-clj"
   :license {:name "MIT"
